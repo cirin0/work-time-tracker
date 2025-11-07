22ca5ecd9a0bb48d94bc1f5a77f27c19b1e738c3
cirin0 <ivan.gruziv@gmail.com>
2025-11-07 21:25:02 +0200

    Implement JWT token refresh functionality with error handling and add corresponding tests

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -5,6 +5,10 @@ namespace App\Services;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Repositories\UserRepository;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthService
@@ -18,7 +22,7 @@ class AuthService
         return $this->repository->register($data);
     }
 
-    public function login(array $credentials): array|null
+    public function login(array $credentials): ?array
     {
         if (!$token = JWTAuth::attempt($credentials)) {
             return null;
@@ -38,9 +42,32 @@ class AuthService
         try {
             JWTAuth::invalidate(JWTAuth::getToken());
             return true;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
     }
 
+
+    public function refresh(): JsonResponse|array
+    {
+        try {
+            $token = JWTAuth::getToken();
+
+            if (!$token) {
+                return response()->json(['error' => 'Token not provided'], 401);
+            }
+            $newToken = JWTAuth::refresh($token);
+            JWTAuth::setToken($newToken);
+            $user = JWTAuth::toUser($newToken);
+
+            return [
+                'access_token' => $newToken,
+                'expires_in' => auth()->factory()->getTTL() * 60,
+                'user' => new UserResource($user),
+            ];
+        } catch (TokenExpiredException|TokenInvalidException|Exception $e) {
+            throw $e;
+
+        }
+    }
 }
