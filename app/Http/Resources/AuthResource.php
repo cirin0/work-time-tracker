59a93bf024f53f2559595447fcd1191342fde1eb
cirin0 <ivan.gruziv@gmail.com>
2026-03-02 18:55:41 +0200

    Refactor: enhance AuthResource to include user role and avatar URL in the response

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class AuthResource extends JsonResource
 {
@@ -13,6 +14,8 @@ class AuthResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'role' => $this->role,
+            'avatar' => $this->avatar ? Storage::url($this->avatar) : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
